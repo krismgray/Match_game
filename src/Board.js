16aa72fe4cb8bbcc2cf0692016acadf83ebadceb
krismgray <kris.m.gray@gmail.com>
2019-10-06 23:54:11 -0600

    changin size

--- src/Board.js
+++ src/Board.js
@@ -13,8 +13,8 @@ export default function Board({ cards, flipped, handleClick, dimension, disabled
             key={card.id}
             id={card.id}
             type={card.type}
-            width={dimension / 6.6}
-            height={dimension / 5.8}
+            width={dimension / 6.2}
+            height={dimension / 5.4}
             flipped={flipped.includes(card.id)}
             handleClick={handleClick}
             disabled={disabled || solved.includes(card.id)}
